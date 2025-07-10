8b4190feb1ee93aee89a94099d29eca870cc45cc
piyush-scripts <kumar.piyush1312@gmail.com>
2025-07-10 20:24:11 +0530

    fixing prisma client error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,6 @@
-import { PrismaClient } from "./generated/prisma";
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from './generated/prisma'
 
-// const globalForPrisma = globalThis as unknown as {
-//     prisma: PrismaClient | undefined;
-// };
-
-// export const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-// if(process.env.NODE_ENV !== 'production') {
-//     globalForPrisma.prisma = prisma ;
-// }
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
+const prisma = new PrismaClient({ adapter })
 export default prisma

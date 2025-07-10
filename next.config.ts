7ddb8dc8af19fa2f47e065ce9e62733b425244c7
0xMukesh <hey.mukesh272921@gmail.com>
2025-07-10 21:26:45 +0530

    test

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,16 @@
 import type { NextConfig } from "next";
+// @ts-ignore
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains :['i.ibb.co','images.unsplash.com']
-  }
+    domains: ["i.ibb.co", "images.unsplash.com"],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) config.plugins = [...config.plugins, new PrismaPlugin()];
+    return config;
+  },
 };
 
 export default nextConfig;

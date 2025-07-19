3f2dfc3279d2ca10bf09c3daf9473d27db75397e
piyush-scripts <kumar.piyush1312@gmail.com>
2025-07-19 09:34:08 +0530

    eslint.config.mjs fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,20 +13,4 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
-// Add rules using FlatConfig style
-eslintConfig.push({
-  rules: {
-    // Allow usage of the `object` type
-    "@typescript-eslint/ban-types": [
-      "error",
-      {
-        extendDefaults: true,
-        types: {
-          object: false,
-        },
-      },
-    ],
-  },
-});
-
 export default eslintConfig;
